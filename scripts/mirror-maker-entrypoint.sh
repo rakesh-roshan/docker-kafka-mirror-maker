87c9c8f0d3bea9ccd0dcd715bcf18f471c18d5c7
rakesh.roshan007@gmail.com <rakesh.roshan007@gmail.com>
2018-09-18 18:42:54 -0400

    Added external kafka support

--- scripts/mirror-maker-entrypoint.sh
+++ scripts/mirror-maker-entrypoint.sh
@@ -4,4 +4,4 @@ echo "Starting Kafka Mirror maker"
 cd /opt/kafka_2.11-0.10.0.1 && \
 bin/kafka-mirror-maker.sh --consumer.config config/consumer.properties \
 --producer.config config/producer.properties \
---whitelist "mirror.maker.topic"
+--whitelist ".*heartbeat,.*gps,.*alarm"
